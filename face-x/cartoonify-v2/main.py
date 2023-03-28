a61d886edb1d423ca5ce764d16e0b440c72fa598
crispengari <Crispengari@gmail.com>
2023-03-28 07:14:44 +0200

    cartoon-images

---
+++ face-x/cartoonify-v2/main.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('me.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, 5)
+edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+color = cv2.bilateralFilter(image, 9, 250, 250)
+cartoon = cv2.bitwise_and(color, color, mask=edges)
+
+images = np.hstack([image, cartoon])
+cv2.imshow("Images", images)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
